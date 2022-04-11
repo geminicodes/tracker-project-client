950838796ba073debd5bb2b1f50db488b6a33168
mValentinaDev <70847744+mValentinaDev@users.noreply.github.com>
2022-04-11 18:51:06 +0300

    pre-auth commit

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import useStyles from './styles';
-import Post from './Post/POst';
+import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import Post from './Post/Post';
 
-function Posts() {
+function Posts({ setCurrentId }) {
     const classes = useStyles();
+    const posts = useSelector((state) => state.posts);
   return (
-      <>
-        <h1>Posts</h1>
-        <Post />
-        <Post />
-    </>
+    !posts.length ? <CircularProgress /> : (
+        <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+          {posts.map((post) => (
+            <Grid key={post._id} item xs={12} sm={6}>
+              <Post post={post} setCurrentId={setCurrentId}/>
+            </Grid>
+          ))}
+        </Grid>
+      )
   )
 }
 
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,9 +1,15 @@
+import { CREATE, UPDATE, DELETE, FETCH_ALL }  from '../constants/actionTypes';
+
 export default (posts = [], action) => {
     switch (action.type) {
-        case 'FETCH_ALL':
-            return posts;
-        case 'CREATE':
-            return posts;
+        case DELETE:
+            return posts.filter((post) => post._id !== action.payload);
+        case FETCH_ALL:
+            return action.payload;
+        case UPDATE:
+            return posts.map((post) => (post._id === action.payload._id ? action.payload : post))
+        case CREATE:
+            return [...posts, action.payload];
         default:
             return posts;
     }
